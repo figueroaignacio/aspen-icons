b000ee95d2da397eb68137fa3a9cc1109c633993
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-06-28 16:54:08 -0300

    fix: fixing some code issues

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,16 +1,10 @@
 // Components
 import { BackButton } from "@/components/BackButton";
 
-// Icons
-import { QuestionMarkCircledIcon } from "@radix-ui/react-icons";
-
 export default function NotFound() {
   return (
     <section className="min-h-[90vh] flex justify-center items-center container">
       <div className="flex flex-col justify-center items-center gap-3 text-center border-[.0625rem] rounded-md p-16 border-dashed">
-        <div className="border-[1px] rounded-full p-3 border-dashed">
-          <QuestionMarkCircledIcon />
-        </div>
         <div>404</div>
         <div>
           <p className="opacity-75">Page not found</p>
--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -3,11 +3,6 @@
 // Next
 import { useRouter } from "next/navigation";
 
-// Components
-
-// Icons
-import { ArrowLeftIcon } from "@radix-ui/react-icons";
-
 interface GoBackProps {
   title: string;
 }
@@ -19,7 +14,6 @@ export function BackButton({ title }: GoBackProps) {
       className="items-center flex gap-2 relative group border-[.0625rem] border-gray/25"
       onClick={() => router.back()}
     >
-      <ArrowLeftIcon className="transition-transform ease-in-out duration-300 transform translate-x-0 group-hover:-translate-x-1" />
       <p>{title}</p>
     </button>
   );
